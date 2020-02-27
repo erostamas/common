3f934b49628e07007ee028b575e01c863e21acf9
Tamas Eros <tamas.eros1@gmail.com>
2020-02-27 15:57:40 +0100

    Deployment image build and minor mods
    Change-Id: I35f3c7750d44db4e86437170222607256f9e8e4a

---
+++ env/buildenv/ubuntu/build_dockerized.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+ROOT_DIR=$(git rev-parse --show-toplevel)
+BRANCH=${GERRIT_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+
+CMD='cd /workspace/common && mkdir -p build && rm -rf build/* && cd build && cmake ../ && make -j'
+
+IMAGE_NAME="erostamas/common_build_ubuntu_$BRANCH"
+
+echo "Building in image: $IMAGE_NAME"
+
+docker pull "$IMAGE_NAME"
+docker run -v $ROOT_DIR:/workspace/common "$IMAGE_NAME" /bin/bash -c "${CMD}"
