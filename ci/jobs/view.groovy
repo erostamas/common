abd2c2243e41323426e2802da43e18adf575c724
Tamas Eros <tamas.eros1@gmail.com>
2020-03-01 14:46:32 +0100

    Jenkins view for all jobs
    Change-Id: I85366a11248cd2d8835a1aa93b5d0852e6e1c263

--- ci/jobs/view.groovy
+++ ci/jobs/view.groovy
@@ -2,6 +2,23 @@ sectionedView("common_${BRANCH_NAME}") {
     filterExecutors()
     sections {
         listView {
+            name('HEAD')
+            alignment('LEFT')
+            width('HALF')
+            jobs {
+                name("common_library_build_${BRANCH_NAME}")
+                name("common_build_image_build_${BRANCH_NAME}")
+                name("common_deployment_image_build_${BRANCH_NAME}")
+            }
+            columns {
+                status()
+                weather()
+                name()
+                lastBuildNode()
+                lastBuildConsole()
+            }
+        }
+        listView {
             name('Gerrit - Review')
             alignment('LEFT')
             width('HALF')
